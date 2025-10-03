bec9b8aab296887da3819a113254b6b5f446ac1a
Bojun Zhang <1002129979@qq.com>
2025-10-03 11:14:13 +0800

    .\learning_plt\figure.py Update

--- Example/mixing_elbow_settings_api/learning_plt/figure.py
+++ Example/mixing_elbow_settings_api/learning_plt/figure.py
@@ -1,4 +1,4 @@
-
+import matplotlib.pyplot as plt
 ###############################################################################
 # prepare figure
 # ~~~~~~~~~~~~~~
@@ -6,13 +6,16 @@
 
 fig, ax = plt.subplots(figsize=figsize)
 cf = ax.contourf(X, Y, V, levels=levels, cmap=cmap)
-cbar = fig.colorbar(cf, ax=ax)
+cbar = fig.colorbar(cf, ax=ax, fraction=0.05, pad=0.02, aspect=30)
 cbar.set_label("Velocity magnitude (m/s)")
 
 # plt.subplots: 创建 figure 与 axes，大小由 figsize 决定。  
 # ax.contourf: 用 X,Y,V 画填色等值线图（colormap 由 cmap，等值面数量由 levels 控制）。
-#  返回的 cf 是 QuadContourSet
-# fig.colorbar: 添加颜色条（legend），并标注单位（m/s
+# 返回的 cf 是 QuadContourSet
+# fraction=0.05：colorbar 比较窄（约占主轴高度的 5% 用于 colorbar 区域）。
+# pad=0.02：主图与 colorbar 间隔较小。
+# aspect=30：使色条更细长（调整视觉）。
+# 
 
  
 ax.set_xlabel("X (m)")
@@ -26,5 +29,10 @@ ax.set_aspect('equal', adjustable='box')
 
 
 
+###############################################################################
+# colormap
+# ~~~~~~~~
+# "viridis"（默认，蓝→绿→黄）  
+#
 
-
+cmp = ["viridis", "jet",  "cividis"]
