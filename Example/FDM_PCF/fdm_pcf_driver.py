2c79c6f41747cd3faa54d33c4e4f8d5836a599f5
Bojun Zhang <1002129979@qq.com>
2025-10-04 17:31:41 +0800

    Just update

--- Example/FDM_PCF/fdm_pcf_driver.py
+++ Example/FDM_PCF/fdm_pcf_driver.py
@@ -1,12 +1,19 @@
-###########################
+###############################################################################
 # Perform required imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~
 # downloading, importing, geometry file
 
+
 import os
-import logging
+import numpy as np
+import matplotlib.pyplot as plt
 import ansys.fluent.core as pyFluent
-from ansys.fluent.visualization import Contour, GraphicsWindow
-from utils import setup_logger, get_colors
+from ansys.fluent.core import SurfaceDataType, SurfaceFieldDataRequest
+from ansys.fluent.visualization import Contour, GraphicsWindow, PlaneSurface
+from ansys.fluent.core.solver import VelocityInlet
+from colorama import Fore, Style
+from utils import project_to_plane, plot_velocity_contour
+from other_utils import setup_logger, get_colors
 from colorama import Fore, Style
 from datetime import datetime
 
@@ -34,6 +41,7 @@ solver_session = pyFluent.launch_fluent(
     mode="solver",
 )
 
+cwd = os.getcwd()
 print(solver_session.get_fluent_version())
 
 ###############################################################################
@@ -154,10 +162,93 @@ solver_session.settings.solution.initialization.hybrid_initialize()
 solver_solution = solver_session.settings.solution
 solver_solution.run_calculation.iterate(iter_count=100)
 
+#######################################################################################
+# Save the case file
+# ~~~~~~~~~~~~~~~~~~
+#
+
+solver_session.settings.file.write(file_type="case-data", file_name="FDM-PCF.cas.h5")
+
+###############################################################################
+# Field_data Module
+# ~~~~~~~~~~~~~~~~~
+# Take a flat, perpendicular outlet plane
+#
+
+# Create an istance of the FieldData class
+# the normal_data is a area vector
+field_data = solver_session.fields.field_data
+
+face_data_request = SurfaceFieldDataRequest(
+        surfaces=["outlet"],
+        data_types=[SurfaceDataType.FacesNormal,
+                    SurfaceDataType.FacesCentroid,
+                   ]
+        )
+all_data = field_data.get_field_data(face_data_request)["outlet"]
+
+normal_data = all_data.face_normals
+normal_mean = normal_data.mean(axis=0)
+normal_unit = normal_mean / np.linalg.norm(normal_mean)
+print(normal_data.shape)
+
+# Get centroid data
+centroid_data = all_data.face_centroids
+centroid_mean = centroid_data.mean(axis=0)
+print(centroid_data.shape)
+
+###############################################################################
+# Get Solution Info
+# ~~~~~~~~~~~~~~~~~
+#
+
+# Get Solution Variable Info
+solution_variable_info = solver_session.fields.solution_variable_info
+zones_info = solution_variable_info.get_zones_info() 
+print("Domains:", zones_info.domains)      # e.g. ['mixture'] 
+print("Zones:", zones_info.zones)          # e.g. ['inlet','wall','outlet',...] 
+domain_name = "mixture"                    # change to domains in your case
+zone_names = ["outlet"]                     # change to zones in your case 
+
+vars_info = solution_variable_info.get_variables_info(zone_names=zone_names, domain_name=domain_name)
+print("Available SVAR names:", vars_info.solution_variables)  # e.g. ['SV_U', 'SV_V', 'SV_P', 'SV_W']
+svu_info = vars_info['SV_U'] 
+svv_info = vars_info['SV_V'] 
+svw_info = vars_info['SV_W'] 
+svcentroid_info = vars_info['SV_CENTROID'] 
+print(svu_info.name, svu_info.dimension, svu_info.field_type) # SV_U 1 <class 'numpy.float64'> 
+
+# Get Solution Variable Data 
+# The centroid data in SV is the same as field data
+solution_variable_data = solver_session.fields.solution_variable_data  
+sv_u = solution_variable_data.get_data(variable_name="SV_U", zone_names=zone_names, domain_name=domain_name)['outlet'] 
+sv_v = solution_variable_data.get_data(variable_name="SV_V", zone_names=zone_names, domain_name=domain_name)['outlet']
+sv_w = solution_variable_data.get_data(variable_name="SV_W", zone_names=zone_names, domain_name=domain_name)['outlet']
+outlet_position = solution_variable_data.get_data(variable_name="SV_CENTROID", zone_names=zone_names, domain_name=domain_name)['outlet']
+outlet_position = np.reshape(outlet_position, (-1, 3))
+
+outlet_vel = np.stack((sv_u, sv_v, sv_w), axis=1)
+outlet_vel_mag = np.linalg.norm(outlet_vel, axis=-1)
+print(outlet_vel.shape)
+print(outlet_vel[1][:])
+
+print(outlet_position.shape)
+print(outlet_position[1][:])
+
+
 ###############################################################################
 # Result module: Configure graphics picture export
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
+
+# Create plane surface using normal point and centroid point
+# Do not know how to use
+outlet_plane = PlaneSurface.create_from_point_and_normal(
+        solver = solver_session,
+        point=centroid_mean,
+        normal=normal_unit
+        )
+
 
 solver_results = solver_session.settings.results
 graphics = solver_results.graphics
@@ -168,13 +259,6 @@ if graphics.picture.use_window_resolution.is_active():
 graphics.picture.x_resolution = 1920
 graphics.picture.y_resolution = 1440
 
-#######################################################################################
-# Save the case file
-# ~~~~~~~~~~~~~~~~~~
-#
-
-solver_session.settings.file.write(file_type="case-data", file_name="FDM-PCF.cas.h5")
-
 ###############################################################################
 # Post-Processing Workflow
 # ~~~~~~~~~~~~~~~~~~~~~~~~
@@ -182,29 +266,62 @@ solver_session.settings.file.write(file_type="case-data", file_name="FDM-PCF.cas
 # Create a contour of velocity magnitude, show and save
 #
 
-# filed: denotes the variable type, such as velocity
-# surfaces_list: select the surface which is need to display
-# node_values: True is smoother; False is cell denote, blocker
-# display(): Necessary,display the picture in the graph
-# range_options: both of them false means take the colorbar according to
-#   the local min and max value
-#graphics = solver_results.graphics
-#graphics.contour["velocity_outlet"] = {
-#        "field": "velocity-magnitude",
-#        "surfaces_list": ["outlet"],
-#        "node_values": False,
-#        }
-#velocity_outlet = solver_results.graphics.contour["velocity_outlet"]
-#velocity_outlet.range_options = {
-#                    "auto_range": True
-#                }
-#velocity_outlet.print_state()
-#velocity_outlet.display()
-#
-#graphics.views.restore_view(view_name="front")
-#graphics.views.auto_scale()
-#graphics.picture.save_picture(file_name="outlet_surf_velocity_magnitude.png")
+solver_results = solver_session.results
 
+graphics = solver_results.graphics
+graphics.contour["velocity_outlet"] = {
+        "field": "velocity-magnitude",
+        "surfaces_list": ["outlet"],
+        "node_values": True,
+        }
+velocity_outlet = solver_results.graphics.contour["velocity_outlet"]
+velocity_outlet.range_options = {
+                    "auto_range": True
+                }
+
+velocity_outlet.print_state()
+velocity_outlet.display()
+
+graphics.views.restore_view(view_name="front")
+graphics.views.auto_scale()
+graphics.picture.save_picture(file_name="outlet_surf_velocity_magnitude.png")
+
+
+###############################################################################
+# Post-Processing Workflow
+# ~~~~~~~~~~~~~~~~~~~~~~~~
+# Draw a outlet velocity profile by plt
+#
+
+save_dir = os.path.join(cwd, "figure")
+os.makedirs(save_dir, exist_ok=True)
+save_path = os.path.join(save_dir, "vel_mag.png")
+coords2d, axis1, axis2, origin = project_to_plane(outlet_position, normal_unit, centroid_mean)
+
+# save and load
+np.savetxt(os.path.join(cwd, "pts.txt"), coords2d, fmt="%.6e", delimiter=" ")
+np.savetxt(os.path.join(cwd, "vel_mag.txt"), outlet_vel_mag, fmt="%.6e")
+pts = np.loadtxt(os.path.join(cwd, "pts.txt"))
+vel_mag = np.loadtxt(os.path.join(cwd, "vel_mag.txt"))
+
+
+# Easy mode
+#fig, ax = plot_velocity_contour(coords2d, outlet_vel_mag)
+
+# Hard mode
+np.multiply(coords2d, 1000, out=coords2d)
+fig, ax = plot_velocity_contour(
+        points=coords2d, vel=outlet_vel_mag, 
+        cmap="viridis", fill_nan_method="nearest",   
+        grid_res=200, smooth_sigma=0.5, 
+        figsize=(8,4), levels=50)
+
+if os.path.exists(save_path):
+    os.remove(save_path)
+
+fig.savefig(save_path, dpi=300, bbox_inches='tight')
+plt.show()
+plt.close(fig)
 
 ###############################################################################
 # Close Fluent
